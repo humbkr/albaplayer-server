91e77aa6fa6778bd110eddd33ca36e8a30f6d1b1
humbkr <humbkr@gmail.com>
2019-01-18 14:36:03 +0100

    Make searching for file extension case-insensitive.

--- internal/alba/interfaces/repository_fs_local.go
+++ internal/alba/interfaces/repository_fs_local.go
@@ -106,7 +106,7 @@ func scanDirectory(path string, dbTransaction *gorp.Transaction) (err error) {
 		if file.IsDir() {
 			// Recursion.
 			scanDirectory(filePath, dbTransaction)
-		} else if matched, _ := filepath.Match("*.mp3", file.Name()); matched {
+		} else if matched, _ := filepath.Match("*.mp3", strings.ToLower(file.Name())); matched {
 			// Get ID3 metadata and add it to an array.
 			metadata, err := getMetadataFromFile(filePath)
 			if err == nil {
